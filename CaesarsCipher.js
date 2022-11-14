52ed4e9f9ce6ae82a794b5495edb4cc5af494850
Alexandr Volkovich <volkovich.a@edu.spbstu.ru>
2022-11-15 00:19:48 +0300

    Create a ROT13 encoded string
    One of the simplest and most widely known ciphers is a Caesar cipher, also known as a shift cipher. In a shift cipher the meanings of the letters are shifted by some set amount.
    A common modern use is the ROT13 cipher, where the values of the letters are shifted by 13 places. Thus A ↔ N, B ↔ O and so on.
    This function takes a ROT13 encoded string as input and returns a decoded string.
    All letters will be uppercase. Do not transform any non-alphabetic character (i.e. spaces, punctuation), but do pass them on.

---
+++ CaesarsCipher.js
@@ -0,0 +1,16 @@
+function rot13(str) {
+  let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let decodedString = "";
+
+  for (let charIndex in str) {
+    if (alphabet.includes(str[charIndex])) {
+      let newIndex = alphabet.indexOf(str[charIndex]) - 13;
+      if (newIndex < 0)
+        newIndex += 26;  
+      decodedString += alphabet[newIndex];
+    } else {
+      decodedString += str[charIndex];
+    }
+  }
+  return decodedString;
+}
