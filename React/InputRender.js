7ef81b6078c9851cfe8d543a29d2ca2ec851757b
Alexandr Volkovich <volkovich.a@edu.spbstu.ru>
2023-01-23 21:52:36 +0300

    Update InputRender.js

--- React/InputRender.js
+++ React/InputRender.js
@@ -15,7 +15,7 @@ class MyApp extends React.Component {
     return (
        <div>
         <GetInput input={this.state.inputValue} handleChange={this.handleChange}/>
-        <RenderInput input={this.state.inputValue} />
+        <RenderInput input={this.state.inputValue}/>
        </div>
     );
   }
