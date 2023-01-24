675f27021f40779977129aaa50ee2140a1d41b9a
Alexandr Volkovich <volkovich.a@edu.spbstu.ru>
2023-01-24 22:32:35 +0300

    Update Reducer.js

--- Redux/Reducer.js
+++ Redux/Reducer.js
@@ -3,12 +3,10 @@ const defaultState = {
 };
 
 const reducer = (state = defaultState, action) => {
-  // Change code below this line
   if (action.type == "LOGIN")
     return {login: true}
   else
     return state
-  // Change code above this line
 };
 
 const store = Redux.createStore(reducer);
